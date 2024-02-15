60fd854f2164eae9fa1dd4b6805d9d30b002793d
Morgan Jones <me@morganlabs.dev>
2024-02-15 19:07:30 +0000

    package scopes

--- main.go
+++ main.go
@@ -1,35 +1,27 @@
+// The `package main` line indicates we are creating a package named main.
+// This acts as the entry package to our file.
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
+// There can only be ONE functon called `main` in an entire go program.
+// This is the beginning of our program. `main` cannot exist in any other
+// files.
 func main() {
-	// When using multiple return types, similarly to a loop through an
-	// array/slice, you must account for every returned value. Use an
-	// underscore to discard the value.
-	firstInitial1, lastInitial1 := getInitials("Morgan Jones")
-	firstInitial2, _ := getInitials("Morgan")
+	// Here, we're going to use the function `greet` which is defined in greetings.go
+	// which is under the same package name `main`.
+	greet("Morgan")
 
-	fmt.Printf("%v%v\n", firstInitial1, lastInitial1)
-	fmt.Printf("%v\n", firstInitial2)
+	// And we're also going to access the `points` slide, which is also defined in
+	// greetings.go
+	for _, points := range points {
+		fmt.Println(points)
+	}
 }
 
-// Multiple return values are signified by making the return type:
-// (type, type, type, ...)
-func getInitials(name string) (string, string) {
-	name = strings.ToUpper(name)
-	names := strings.Split(name, " ")
-
-	var initials []string
-	for _, name := range names {
-		initials = append(initials, name[:1])
-	}
-
-	if len(initials) == 1 {
-		return initials[0], "_"
-	}
-
-	return initials[0], initials[1]
-}
+// Currently at this stage in the course I am watching, we're running these go files with
+// `go run main.go`
+// However, doing this will result in go telling us that `points` and `greet` are not defined.
+// To fix this, we need to include every file inside of the `main` package for their vairables
+// and functions to be exposed to us:
+// `go run main.go greetings.go`
