04fce644de3461f0c7fb02d8f4241f4082a65343
Morgan Jones <me@morganlabs.dev>
2024-02-15 17:41:08 +0000

    if statements

--- main.go
+++ main.go
@@ -6,28 +6,10 @@ import (
 
 func main() {
 	age := 17
-	rights := calculateRights(age)
-	canDrive, canDrink, canDrinkUS := rights[0], rights[1], rights[2]
 
-	fmt.Printf("This person is %v-years-old. This means that they can:\nDrive: %v,\nDrink: %v,\nDrink in the US: %v\n", age, canDrive, canDrink, canDrinkUS)
-
-	age = 18
-	rights = calculateRights(age)
-	canDrive, canDrink, canDrinkUS = rights[0], rights[1], rights[2]
-
-	fmt.Printf("This person is %v-years-old. This means that they can:\nDrive: %v,\nDrink: %v,\nDrink in the US: %v\n", age, canDrive, canDrink, canDrinkUS)
-
-	age = 21
-	rights = calculateRights(age)
-	canDrive, canDrink, canDrinkUS = rights[0], rights[1], rights[2]
-
-	fmt.Printf("This person is %v-years-old. This means that they can:\nDrive: %v,\nDrink: %v,\nDrink in the US: %v\n", age, canDrive, canDrink, canDrinkUS)
-}
-
-func calculateRights(age int) [3]bool {
-	canDrive := age >= 17
-	canDrink := age >= 18
-	canDrinkUS := age >= 21
-
-	return [3]bool{canDrive, canDrink, canDrinkUS}
+	if age >= 18 {
+		fmt.Println("This person is a legal adult.")
+	} else {
+		fmt.Println("This person is a minor.")
+	}
 }
