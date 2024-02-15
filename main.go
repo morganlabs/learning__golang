7e597300c7c56679c40b4fc56524e55785ebb32f
Morgan Jones <me@morganlabs.dev>
2024-02-15 16:00:45 +0000

    formatting strings

--- main.go
+++ main.go
@@ -3,33 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	var ageOne int = 16 // AT MINIMUM 32-bits, but this is NOT the same as int32.
-	var ageTwo = 17
-	ageThree := 18
+	fmt.Print("Hello,")          // This prints WITHOUT a newline at the end. This will be printed on the same line as ...
+	fmt.Print(" world.\n")       // If we want to end with a newline, we need to add \n to the end. Or use...
+	fmt.Println("Hello, world.") // This, however, adds a newline
 
-	fmt.Println(ageOne, ageTwo, ageThree)
+	// Concatenation
+	name := "Morgan"
+	age := 16
+	fmt.Println("Hello! My name is", name, "and I am", age, "years old.") // Go automatically adds spaces
+	// Or we can use Printf (if you want a newline at the end you must add it yourself):
+	// %_ is a "format specifier"
+	fmt.Printf("Hello! My name is %v and I am %v-years-old.\n", name, age) // %v is a variable placeholder
+	fmt.Printf("Hello! My name is %q and I am %v-years-old.\n", name, age) // %q is a placeholder that surrounds the variables with quotation marks*
+	fmt.Printf("name is of type %T\n", name)                               // %T gives you the type of variable
+	fmt.Printf("age is of type %T\n", age)
 
-	// Bits
-	var numOne int8 = 25    // 8-bits
-	var numTwo int16 = 25   // 8-bits
-	var numThree int32 = 25 // 8-bits
-	var numFour int64 = 25  // 8-bits
-	var numFive int8 = 25   // 8-bits
-	var numSix int8 = 25    // 8-bits
-	fmt.Println(numOne, numTwo, numThree, numFour, numFive, numSix)
+	score := 492.55
+	fmt.Printf("You scored %f points!\n", score)    // %f is the qualifier for a float, however it will be followed by multiple zeroes
+	fmt.Printf("You scored %0.2f points!\n", score) // We can limit the number of zeroes with %0.xf
+	fmt.Printf("You scored %0.1f points!\n", score) // Rounding even works!
+	// * This will not work properly with anything other than strings.
 
-	// Unsigned ints
-	// These can only be positive numbers
-	// This is useful for when you need to use lower bits (say, 8-bits), but need a larger number.
-	// Because the negatives aren't acounted for, int8 can go down to -128 and up to *127*,
-	// but uint8 can go down to 0 and up to *255*.
-
-	// Floats
-	// These are limited to float32 and float64, so 32- and 64-bits relatively
-	// These allow you to have a decimal place in a number
-	// Floats are SIGNED.
-	// f64 has a higher precision than f32, and for this reason, when Go infers the type of a float, f64 is the default.
-	var floatOne float32 = 293018348433948938938983478945782748372.12
-	var floatTwo float64 = 45738943728947274382973478934728947289473289428947238947238953429812357892238943278957348972423758923475892347892334287432789473289472389574289427890543728957377.21 // this isnt even the limit of a 64-bit float...
-	fmt.Println(floatOne, floatTwo)
+	// We can also use the Sprintf function.
+	// However, instead of printing to the console, the output gets saved to a variable
+	// Like template literals, kinda
+	firstName := "Morgan"
+	lastName := "Jones"
+	fullName := fmt.Sprintf("%v %v", firstName, lastName)
+	fmt.Println(fullName)
 }
