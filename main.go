ec04d2b4669e31a5daf44b308b88ff8a18206dec
Morgan Jones <me@morganlabs.dev>
2024-02-15 04:12:26 +0000

    strings

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+	var nameOne string = "Morgan" // Explicitly state type
+	var nameTwo = "Nagrom"        // Infer type
+	var nameThree string          // Uninitialised variable, type MUST be specified
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "Oranmg"
+	nameThree = "Ganmor"
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "Romnag" // A shorthand for initialising a variable w/o declaring it's type
+	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
 }
