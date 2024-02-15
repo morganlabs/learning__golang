45234f0264b134300319bbf9279b6d9e114f65af
Morgan Jones <me@morganlabs.dev>
2024-02-15 16:41:05 +0000

    std: strings

--- main.go
+++ main.go
@@ -1,71 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	// Initialise an array
-	// Arrays are **fixed length**. You cannot add more to an array
-	// than the length described at initialisation, however you may
-	// have less items inside. And, of course, you cannot deviate
-	// from the type you described. You cannot have an array of
-	// strings and ints
+	name := "Morgan"
+	greeting := fmt.Sprintf("Hello, %v!", name)
 
-	// Explicitly type an array
-	var ages1 [6]int = [6]int{16, 23, 38, 42, 59, 60}
+	// NOTE: All of these methods are NOT destructive, as in they
+	// do NOT modify the original string input. Thee values must
+	// either be saved to a new variable, printed or saved over
+	// the original variable to be useful
 
-	// Infer the type (and length) of an array
-	var ages2 = [6]int{16, 23, 38, 42, 59, 60}
-	ages3 := [6]int{16, 23, 38, 42, 59, 60}
+	// We can import "strings" and use strings.Contains to find out
+	// if a string contains a substring.
+	// Here we are checking if `greeting` contains `name`, which will
+	// always be true.
+	containsName1 := strings.Contains(greeting, name)
 
-	// Initialise an empty array, ready for data
-	// If this is printed without any data, the number 0 will be
-	// repeated for the length of the array.
-	var ages4 [6]int = [6]int{}
+	// But here, we're checking if "Jack" exists. This returns false
+	containsName2 := strings.Contains(greeting, "Jack")
 
-	// Chage the values inside an array
-	// This works
-	ages2[0] = 17
+	// However, if we change the name to Jack and reformat the greeting
+	// string...
+	name = "Jack"
+	greeting = fmt.Sprintf("Hello, %v!", name)
 
-	// However, this does not, as the array only goes up to index 6
-	// ages2[7] = 17
+	// This returns true
+	containsName3 := strings.Contains(greeting, "Jack")
+	fmt.Println(containsName1, containsName2, containsName3)
 
-	fmt.Println(ages1, ages2, ages3, ages4)
+	// We can also use strings.Replace and .ReplaceAll to replace either
+	// n or all instances of a substring inside of a string
+	// .Replace requires you to specify the number of instances to replace
+	// at the end of the method
+	noMoreJack := strings.Replace(greeting, "Jack", "Morgan", 1)
+	fmt.Println(noMoreJack)
 
-	// Print the length of an array using the len() method
-	fmt.Println(len(ages1))
+	// And we can use .ReplaceAll to replace every instance
+	greeting = "Hello, Earth. Hello, Earth. Hello, Earth."
+	noMoreEarth := strings.ReplaceAll(greeting, "Earth", "Mars")
+	fmt.Println(noMoreEarth)
 
-	// Slides
-	// These use the array type under the hood.
-	// Slices are of a dynamic length, unlike an array
-	var scores = []int{100, 200, 300, 400}
+	// There's also .ToUpper and .ToLower to convert a string to
+	// upper- or lower-case respectively
+	noMoreEarthShouting := strings.ToUpper(noMoreEarth)
+	noMoreEarthWhisper := strings.ToLower(noMoreEarth)
+	fmt.Println(noMoreEarthShouting)
+	fmt.Println(noMoreEarthWhisper)
 
-	// We can modify the values inside of a slice the same way we do with an array
-	scores[1] = 250
+	// Or .Index, which allows us to find the index of a substring
+	whereIsMars := strings.Index(noMoreEarth, "Mars")
+	fmt.Println(whereIsMars)
+	// This will print 7, as the first instance of Mars is found at index 7.
 
-	// However, because they are also of dynamic lengths, we can append to a slice, too
-	// Keep in mind that this append function does NOT modify the original slice,
-	// but instead return a new slice to us. This means we need to assign the new
-	// slice to a new variable, or the original variable if we want to modify it
-	scores = append(scores, 500)
-	newScores := append(scores, 600)
-
-	fmt.Println(scores, newScores)
-
-	// Ranges
-	// These let you grab parts of an array or slice from index X to index Y.
-	// Remember that index X is INclusive and index Y is EXclusive
-	// This grabs ages1[1] and ages[2], but not ages[3] as index Y is EXclusive
-	rangeOne := ages1[1:3]
-
-	// This grabs everything STARTING from index 2 all the way until the end of the array
-	rangeTwo := ages1[2:]
-
-	// This grabs everything from the beginning of the array until index 4 (remember, the second number is EXclusive)
-	rangeThree := ages1[:5]
-
-	// And because ranges are Slices by default (reguardless of whether they were derived from
-	// an array or a slice), we can append to them
-	rangeThree = append(rangeThree, 48)
-
-	fmt.Println(rangeOne, rangeTwo, rangeThree)
+	// Or we can use .Split to split a string into a slice
+	sliceMars := strings.Split(noMoreEarth, " ")
+	fmt.Println(sliceMars)
 }
